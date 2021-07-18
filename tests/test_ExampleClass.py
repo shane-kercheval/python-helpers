8a5eb43593bd6f4499b195c1665d652417180478
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2021-07-17 20:14:19 -0700

    Update test_ExampleClass.py

--- tests/test_ExampleClass.py
+++ tests/test_ExampleClass.py
@@ -7,7 +7,3 @@ class TestExampleClass(unittest.TestCase):
     def test_something(self):
         example = ExampleClass()
         assert example.my_variable == 0
-
-
-if __name__ == '__main__':
-    unittest.main()
