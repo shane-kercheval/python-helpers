5fb448ae096deaffd6d29ebdb9b570f880e04032
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2021-07-20 19:40:32 -0700

    Create date.py

---
+++ helpsk/date.py
@@ -0,0 +1,17 @@
+def format_as_months(dates: datetime?) -> str:
+    pass
+
+def format_as_quarters(dates: datetime?) -> str:
+    pass
+
+def format_as_fiscal_quarters():
+    pass
+
+def floor_months():
+    pass
+
+def floor_quarters():
+    pass
+
+def floor_fiscal_quarters():
+    pass
