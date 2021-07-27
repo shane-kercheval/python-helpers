a5613fcbaa7c29b7add2872c9395854e11a11fc2
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2021-07-26 23:27:20 -0700

    update documentation for `floor()`

--- helpsk/date.py
+++ helpsk/date.py
@@ -31,15 +31,18 @@ def floor(value: Union[datetime.datetime, datetime.date],
     """
     "Rounds" the datetime value down (i.e. floor) to the the nearest granularity.
 
-    For example, if `date` is `2021-03-20` then `floor_quarter` will return `2021-01-01`.
+    For example, if `date` is `2021-03-20` and granularity is `Granularity.QUARTER` then `floor()` will return `2021-01-01`.
 
     If the fiscal year starts on November (`first_fiscal_month is `11`; i.e. the quarter is November,
-    December, January) and the date is `2022-01-28` then `floor_quarter` will return `2021-11-01`.
+    December, January) and the date is `2022-01-28` and granularity is `Granularity.QUARTER` then `floor()` will return `2021-11-01`.
 
     Parameters
     ----------
     value : datetime.datetime
         a datetime
+
+    granularity: Granularity
+        the granularity to round down to (i.e. DAY, MONTH, QUARTER)
 
     first_fiscal_month: int
         Only applicable for `Granularity.QUARTER`. The value should be the month index (e.g. Jan is 1, Feb, 2,
@@ -68,36 +71,3 @@ def floor(value: Union[datetime.datetime, datetime.date],
         return floor(value, granularity=Granularity.MONTH) - relativedelta.relativedelta(months=months_to_subtract)
 
     raise ValueError("Unknown Granularity type")
-
-
-def floor_quarter_old(date: datetime.date, first_fiscal_month: int = 1) -> datetime.date:
-    """
-    "Rounds" the date down (i.e. floor) to the start of the current quarter. 
-
-    For example, if `date` is `2021-03-20` then `floor_quarter` will return `2021-01-01`.
-
-    If the fiscal year starts on November (`first_fiscal_month is `11`; i.e. the quarter is November,
-    December, January) and the date is `2022-01-28` then `floor_quarter` will return `2021-11-01`.
-
-    Parameters
-    ----------
-    date : datetime.date
-        a date
-
-    first_fiscal_month: int
-        the month index (e.g. Jan is 1, Feb, 2, etc.) that corresponds to the first month of the
-        fiscal year.
-
-    Returns
-    -------
-    date - the start of the current quarter
-    """
-    relative_start_index = ((first_fiscal_month - 1) % 3) + 1
-    current_month_index = ((date.month - 1) % 3) + 1
-    months_to_subtract = (((relative_start_index * -1) + current_month_index) % 3)
-
-    return floor_month(date) - relativedelta.relativedelta(months=months_to_subtract)
-
-
-def to_string(date: datetime.date) -> str:
-    return date.strftime("%Y-%m-%d")
