09dcd3e0a5188ca796c30d9bc426123255ebb411
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2021-07-26 23:19:25 -0700

    Update date.py

--- helpsk/date.py
+++ helpsk/date.py
@@ -1,5 +1,15 @@
 import datetime
 from dateutil import relativedelta
+from typing import Union
+
+from enum import unique, Enum, auto
+
+
+@unique
+class Granularity(Enum):
+    DAY = auto()
+    MONTH = auto()
+    QUARTER = auto()
 
 
 def ymd(date_string: str) -> datetime.date:
@@ -14,8 +24,50 @@ def format_as_quarter(dates: datetime.date, first_fiscal_month: int = 1) -> str:
     pass
 
 
-def floor_month(date: datetime.date) -> datetime.date:
-    return date.replace(day=1)
+def floor(value: Union[datetime.datetime, datetime.date],
+          granularity: Granularity = Granularity.DAY,
+          first_fiscal_month: int = 1) -> datetime.date:
+
+    """
+    "Rounds" the datetime value down (i.e. floor) to the the nearest granularity.
+
+    For example, if `date` is `2021-03-20` then `floor_quarter` will return `2021-01-01`.
+
+    If the fiscal year starts on November (`first_fiscal_month is `11`; i.e. the quarter is November,
+    December, January) and the date is `2022-01-28` then `floor_quarter` will return `2021-11-01`.
+
+    Parameters
+    ----------
+    value : datetime.datetime
+        a datetime
+
+    first_fiscal_month: int
+        Only applicable for `Granularity.QUARTER`. The value should be the month index (e.g. Jan is 1, Feb, 2,
+        etc.) that corresponds to the first month of the fiscal year.
+
+    Returns
+    -------
+    date - the date rounded down to the naerest granularity
+    """
+    if granularity == Granularity.DAY:
+        if isinstance(value, datetime.datetime):
+            return value.date()
+        else :
+            return value
+
+    if granularity == Granularity.MONTH:
+        if isinstance(value, datetime.datetime):
+            return value.replace(day=1).date()
+        else :
+            return value.replace(day=1)
+
+    if granularity == Granularity.QUARTER:
+        return ''
+
+    raise ValueError("Unknown Granularity type")
+
+
+
 
 
 def floor_quarter(date: datetime.date, first_fiscal_month: int = 1) -> datetime.date:
@@ -47,5 +99,5 @@ def floor_quarter(date: datetime.date, first_fiscal_month: int = 1) -> datetime.
     return floor_month(date) - relativedelta.relativedelta(months=months_to_subtract)
 
 
-def date_to_string(date: datetime.date) -> str:
+def to_string(date: datetime.date) -> str:
     return date.strftime("%Y-%m-%d")
