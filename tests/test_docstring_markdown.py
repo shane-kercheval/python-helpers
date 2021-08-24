a348c0517046cec252effd32d09dbd8ce94481ae
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2021-08-23 20:55:59 -0700

    Update test_docstring_markdown.py

--- tests/test_docstring_markdown.py
+++ tests/test_docstring_markdown.py
@@ -1,12 +1,12 @@
-import os
 import unittest
-from docstring_markdown import build_markdown
-from helpers import get_test_path
 
-os.getcwd()
+from docstring_markdown import build_markdown
+from tests.helpers import get_test_path
+
+
 # noinspection PyMethodMayBeStatic
 class TestExampleClass(unittest.TestCase):
     def test_something(self):
-        build_markdown.execute_build(input_path='../helpsk',
-                                     output_path= get_test_path() + '/test_files')
-)
+        build_markdown.execute_build(input_path=get_test_path() + '/../helpsk/',
+                                     output_path=get_test_path() + '/test_files')
+
