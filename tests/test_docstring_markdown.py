83b687817aecc4733a992d46372b11983d058d5a
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2021-08-23 20:41:23 -0700

    refactor build_markdown.py

--- tests/test_docstring_markdown.py
+++ tests/test_docstring_markdown.py
@@ -1,9 +1,12 @@
 import os
 import unittest
 from docstring_markdown import build_markdown
+from helpers import get_test_path
 
 os.getcwd()
 # noinspection PyMethodMayBeStatic
 class TestExampleClass(unittest.TestCase):
     def test_something(self):
-        build_markdown.execute_build(path='../helpsk')
+        build_markdown.execute_build(input_path='../helpsk',
+                                     output_path= get_test_path() + '/test_files')
+)
