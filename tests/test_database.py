cab2800b7cbc2a5a14c0ce6d0043fc285bc02e4c
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2021-09-01 09:46:16 -0700

    added snowflake tests and mock

--- tests/test_database.py
+++ tests/test_database.py
@@ -63,22 +63,43 @@ class TestDatabase(unittest.TestCase):
         # contains the proper keyboard arguments
         Snowflake(**config_dict)
 
-    def test_Redshift(self):
+    def test_Database(self):
         redshift_config = RedshiftConfigFile(config_file=self.sample_redshift_file)
+        snowflake_config = SnowflakeConfigFile(config_file=self.sample_snowflake_file)
+
         database_objects = [Redshift(**redshift_config.get_dictionary()),
                             Redshift.from_config(redshift_config),
+                            Snowflake(**snowflake_config.get_dictionary()),
+                            Snowflake.from_config(snowflake_config),
                             ]
         for index, db_object in enumerate(database_objects):
             with self.subTest(index=index, database=type(db_object)):
                 self.assertFalse(db_object.is_open())
                 self.assertIsNone(db_object.connection_object)
 
-                # mock psycopg2.connect so that we can "open" the connection to the database
-                with patch('psycopg2.connect'):
-                    db_object.open()
+                # mock connection method so that we can "open" the connection to the database
+                if isinstance(db_object, Redshift):
+                    with patch('psycopg2.connect'):
+                        db_object.open()
+                else:
+                    with patch('snowflake.connector.connect') as mock_snowflake_connector:
+                        # mock this logic out:
+                        # connect() -> cursor() -> ... -> fetch_pandas_all()
+                        # actual code
+                        # cursor = self.connection_object.cursor()
+                        # cursor.execute(sql)
+                        # dataframe = cursor.fetch_pandas_all()
+                        mock_con = mock_snowflake_connector.return_value
+                        mock_cur = mock_con.cursor.return_value
+                        mock_cur.fetch_pandas_all.return_value = pd.DataFrame()
+
+                        # now open the connection and the db_object.connection_object should be populated with
+                        # the mock object
+                        db_object.open()
 
                 self.assertTrue(db_object.is_open())
                 self.assertIsNotNone(db_object.connection_object)
+                self.assertIsInstance(db_object.connection_object, unittest.mock.MagicMock)
                 db_object.open()  # test that calling open again doesn't not fail
 
                 results = db_object.query("SELECT * FROM doesnt_exist LIMIT 100")
@@ -90,5 +111,5 @@ class TestDatabase(unittest.TestCase):
                 self.assertFalse(db_object.is_open())
                 self.assertIsNone(db_object.connection_object)
 
-    def test_Snowflake(self):
-        pass
+# test open connections after closing connection
+# test context manager "with asdf"
