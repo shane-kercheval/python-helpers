dfdfa12b9409401b64cf60a3ca134bac2f8677dc
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2021-09-09 19:23:44 -0700

    added Colors enum

---
+++ helpsk/color.py
@@ -0,0 +1,48 @@
+"""Provides enums and helper functions related to colors/palettes/etc.
+"""
+from enum import unique, Enum
+
+
+@unique
+class Colors(Enum):
+    """Provides hex values for various colors"""
+
+    PASTEL_BLUE = '#7AA9CF'
+    TULIP_TREE = '#EBB13E'
+    CUSTOM_GREEN = '#41B3A3'
+    CRAIL = '#A65B50'
+    DOVE_GRAY = '#7A7A7A'
+    CUSTOM_PURPLE = '#C38D9E'
+    CADMIUM_ORANGE = '#F28E2B'
+
+    MARINER = '#4E79A7'
+    SANDSTONE = '#F4CC70'
+    AVOCADO = '#258039'
+    TOMATO = '#CF3721'
+    MIST = '#90AFC5'
+    LAVENDER = '#C396E8'
+    TREE_POPPY = '#FD9126'
+
+    CERULEAN = '#1EB1ED'
+    YELLOW_PEPPER = '#F5BE41'
+    PIGMENT_GREEN = '#1AAF54'
+    POPPY = '#FF420E'
+    LOBLOLLY_GRAY = '#B4B7B9'
+    LAVENDER_2 = '#6C648B'
+    FLAMINGO = '#FC641F'
+
+    FOREST = '#1E434C'
+    GOLD = '#C99E10'
+    SUNFLOWER = '#3F681C'
+    CRIMSON = '#8D230F'
+    GRANITE = '#B7B8B6'
+    VIVID_VIOLET = '#932791'
+    PETAL = '#F98866'
+
+    BLACK_SHADOW = '#2A3132'
+    SKY = '#375E97'
+    MEDIUM_SEA_GREEN = '#37B57F'
+    RED_CLAY = '#A43820'
+
+
+GOOD_BAD = (Colors.AVOCADO, Colors.TOMATO)
---
+++ tests/test_color.py
@@ -0,0 +1,83 @@
+import unittest
+import warnings
+
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+from tests.helpers import check_plot, get_test_path
+
+
+def plot_colors(colors, title, sort_colors=True, empty_cols=0):
+    """
+    Code from:
+        https://matplotlib.org/stable/gallery/color/named_colors.html
+    """
+    cell_width = 212
+    cell_height = 22
+    swatch_width = 48
+    margin = 12
+    topmargin = 40
+
+    # Sort colors by hue, saturation, value and name.
+    if sort_colors is True:
+        by_hsv = sorted((tuple(mcolors.rgb_to_hsv(mcolors.to_rgb(color))),
+                         name)
+                        for name, color in colors.items())
+        names = [name for hsv, name in by_hsv]
+    else:
+        names = list(colors)
+
+    n = len(names)
+    nrows = 7
+    # ncols = n // nrows + int(n % nrows > 0)
+    # ncols = 4 - empty_cols
+    # nrows = n // ncols + int(n % ncols > 0)
+
+    width = cell_width * 4 + 2 * margin
+    height = cell_height * nrows + margin + topmargin
+    dpi = 72
+
+    fig, ax = plt.subplots(figsize=(width / dpi, height / dpi), dpi=dpi)
+    fig.subplots_adjust(margin/width, margin/height,
+                        (width-margin)/width, (height-topmargin)/height)
+    ax.set_xlim(0, cell_width * 4)
+    ax.set_ylim(cell_height * (nrows-0.5), -cell_height/2.)
+    ax.yaxis.set_visible(False)
+    ax.xaxis.set_visible(False)
+    ax.set_axis_off()
+    ax.set_title(title, fontsize=24, loc="left", pad=10)
+
+    for i, name in enumerate(names):
+        row = i % nrows
+        col = i // nrows
+        y = row * cell_height
+
+        swatch_start_x = cell_width * col
+        text_pos_x = cell_width * col + swatch_width + 7
+
+        ax.text(text_pos_x, y, name, fontsize=14,
+                horizontalalignment='left',
+                verticalalignment='center')
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            ax.add_patch(
+                Rectangle(xy=(swatch_start_x, y-9), width=swatch_width,
+                          height=18, color=colors[name], edgecolor='0.7')
+            )
+
+    return fig
+
+
+class TestColors(unittest.TestCase):
+
+    def test_adsf(self):
+        from helpsk.color import Colors
+
+        color_names = [e.name for e in Colors]
+        color_values = [e.value for e in Colors]
+
+        check_plot(file_name=get_test_path() + '/test_files/color/colors.png',
+                   plot_function=lambda: plot_colors(dict(zip(color_names, color_values)),
+                                                     title="Colors Enum", sort_colors=False, empty_cols=1))
