4ec0659796b8129914eaf9610de03e8f764793c6
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2021-09-11 17:03:25 -0700

    Update sklearn.py

--- helpsk/sklearn.py
+++ helpsk/sklearn.py
@@ -10,65 +10,6 @@ import helpsk.color as hcolor
 import helpsk.pandas_style as hstyle
 
 
-class TransformerChooser(BaseEstimator, TransformerMixin):
-    """Transformer that wraps another Transformer. This allows different transformer objects to be tuned.
-    """
-    def __init__(self, base_transformer=None):
-        """
-        Args:
-            base_transformer:
-                Transformer object (e.g. StandardScaler, MinMaxScaler)
-        """
-        self.base_transformer = base_transformer
-
-    def fit(self, X, y=None):  # pylint: disable=invalid-name # noqa
-        """fit implementation
-        """
-        if self.base_transformer is None:
-            return self
-
-        return self.base_transformer.fit(X, y)
-
-    def transform(self, X):  # pylint: disable=invalid-name # noqa
-        """transform implementation
-        """
-        if self.base_transformer is None:
-            return X
-
-        return self.base_transformer.transform(X)
-
-
-class CustomOrdinalEncoder(BaseEstimator, TransformerMixin):
-    """First replaces missing values with '<missing>' then applies OrdinalEncoder
-    """
-
-    def __init__(self):
-        self._ordinal_encoder = OrdinalEncoder()  # unknown_value=-1,
-        # handle_unknown='use_encoded_value')
-        self._missing_value = '<missing>'
-
-    def _fill_na(self, X):  # pylint: disable=invalid-name # noqa
-        """Helper function that fills missing values with strings before calling OrdinalEncoder"""
-        for column in X.columns.values:
-            if X[column].dtype.name == 'category':
-                if self._missing_value not in X[column].cat.categories:
-                    X[column] = X[column].cat.add_categories(self._missing_value)
-                X[column] = X[column].fillna(self._missing_value)
-
-        return X
-
-    def fit(self, X, y=None):  # pylint: disable=invalid-name,unused-argument # noqa
-        """fit implementation"""
-        X = self._fill_na(X)  # pylint: disable=invalid-name # noqa
-        self._ordinal_encoder.fit(X)
-        return self
-
-    def transform(self, X):  # pylint: disable=invalid-name # noqa
-        """transform implementation"""
-        X = self._fill_na(X)  # pylint: disable=invalid-name # noqa
-        return self._ordinal_encoder.transform(X)
-
-
 # pylint: disable=too-many-locals
 def cv_results_to_dataframe(cv_results: dict,
                             num_folds: int,
@@ -180,3 +121,62 @@ def cv_results_to_dataframe(cv_results: dict,
         results = None
 
     return results
+
+
+class TransformerChooser(BaseEstimator, TransformerMixin):
+    """Transformer that wraps another Transformer. This allows different transformer objects to be tuned.
+    """
+    def __init__(self, base_transformer=None):
+        """
+        Args:
+            base_transformer:
+                Transformer object (e.g. StandardScaler, MinMaxScaler)
+        """
+        self.base_transformer = base_transformer
+
+    def fit(self, X, y=None):  # pylint: disable=invalid-name # noqa
+        """fit implementation
+        """
+        if self.base_transformer is None:
+            return self
+
+        return self.base_transformer.fit(X, y)
+
+    def transform(self, X):  # pylint: disable=invalid-name # noqa
+        """transform implementation
+        """
+        if self.base_transformer is None:
+            return X
+
+        return self.base_transformer.transform(X)
+
+
+class CustomOrdinalEncoder(BaseEstimator, TransformerMixin):
+    """First replaces missing values with '<missing>' then applies OrdinalEncoder
+    """
+
+    def __init__(self):
+        self._ordinal_encoder = OrdinalEncoder()  # unknown_value=-1,
+        # handle_unknown='use_encoded_value')
+        self._missing_value = '<missing>'
+
+    def _fill_na(self, X):  # pylint: disable=invalid-name # noqa
+        """Helper function that fills missing values with strings before calling OrdinalEncoder"""
+        for column in X.columns.values:
+            if X[column].dtype.name == 'category':
+                if self._missing_value not in X[column].cat.categories:
+                    X[column] = X[column].cat.add_categories(self._missing_value)
+                X[column] = X[column].fillna(self._missing_value)
+
+        return X
+
+    def fit(self, X, y=None):  # pylint: disable=invalid-name,unused-argument # noqa
+        """fit implementation"""
+        X = self._fill_na(X)  # pylint: disable=invalid-name # noqa
+        self._ordinal_encoder.fit(X)
+        return self
+
+    def transform(self, X):  # pylint: disable=invalid-name # noqa
+        """transform implementation"""
+        X = self._fill_na(X)  # pylint: disable=invalid-name # noqa
+        return self._ordinal_encoder.transform(X)
