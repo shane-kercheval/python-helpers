04d17f55b0944476c1f60777b2eaf5475de687aa
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2021-12-30 13:27:59 -0800

    Update test_sklearn_eval.py

--- tests/test_sklearn_eval.py
+++ tests/test_sklearn_eval.py
@@ -40,7 +40,7 @@ class TestSklearnEval(unittest.TestCase):
         credit_data.loc[10:54, ['credit_amount']] = 0
         y_full = credit_data['target']
         X_full = credit_data.drop(columns='target')  # noqa
-        y_full = label_binarize(y_full, classes=['bad', 'good']).flatten()
+        y_full = label_binarize(y_full, classes=['good', 'bad']).flatten()
         X_train, X_test, y_train, y_test = train_test_split(X_full, y_full, test_size=0.2, random_state=42)  # noqa
         del y_full, X_full
         numeric_columns = hlp.pandas.get_numeric_columns(X_train)
