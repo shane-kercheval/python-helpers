ef6b3db2eb9c4a4128e8bdfa6718def47c1273a3
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-02-01 20:11:34 -0800

    Update sklearn_search.py

--- helpsk/sklearn_search.py
+++ helpsk/sklearn_search.py
@@ -1,6 +1,14 @@
+from enum import unique, Enum, auto
+
 from sklearn.compose import ColumnTransformer
 from sklearn.dummy import DummyClassifier
-from enum import unique, Enum, auto
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, OneHotEncoder
+
+import helpsk as hlp
+
 
 @unique
 class ClassifierSearchSpaceModels(Enum):
@@ -8,26 +16,20 @@ class ClassifierSearchSpaceModels(Enum):
     XGBoost = auto()
 
 
-#`XGBoostError: XGBoost Library (libxgboost.dylib) could not be loaded on Apple Silicon (ARM)`
-#https://github.com/dmlc/xgboost/issues/6909
-#```
-#pip install --upgrade --force-reinstall xgboost --no-binary xgboost -v
-#```
-
 # need to make sure this works for a single model or multiple models
-class SklearnClassifierSearchSpace:
+class ClassifierSearchSpace:
     def __init__(self,
                  # remove these and pass data and get column names directly?
-                 numeric_column_names,
-                 non_numeric_column_names,
-                 models=[
+                 data,
+                 models=[  # noqa
                      ClassifierSearchSpaceModels.LogisticRegression,
                      ClassifierSearchSpaceModels.XGBoost
                  ],
-                 iterations = [7, 6]):
+                 iterations=[7, 6]):  # noqa
         assert len(models) == len(iterations)
-        self._numeric_column_names = numeric_column_names
-        self._non_numeric_column_names = non_numeric_column_names
+
+        self._numeric_column_names = hlp.pandas.get_numeric_columns(data)
+        self._non_numeric_column_names = hlp.pandas.get_non_numeric_columns(data)
         self._models = models
         self._iterations = iterations
 
@@ -45,7 +47,7 @@ class SklearnClassifierSearchSpace:
             # e.g. One Hot, Custom-OrdinalEncoder
             ('encoder', hlp.sklearn_pipeline.TransformerChooser()),
         ])
-        # associate numeric/non-numeric columns with cooresponding pipeline
+        # associate numeric/non-numeric columns with corresponding pipeline
         transformations_pipeline = ColumnTransformer([
             ('numeric', numeric_pipeline, self._numeric_column_names),
             ('non_numeric', non_numeric_pipeline, self._non_numeric_column_names)
@@ -57,12 +59,14 @@ class SklearnClassifierSearchSpace:
         ])
 
         return full_pipeline
-    
-    def _build_transformer_search_space(imputer_strategies=['mean', 'median', 'most_frequent'],
+
+    @staticmethod
+    def _build_transformer_search_space(imputer_strategies=['mean', 'median', 'most_frequent'],  # noqa
                                         scaler_min_max=True,
                                         scaler_standard=True,
                                         encoder_one_hot=True,
-                                        encoder_ordineral=True):
+                                        encoder_ordinal=True):
+        from skopt.space import Categorical
 
         if imputer_strategies:
             imputers = [SimpleImputer(strategy=x) if x else None for x in imputer_strategies]
@@ -75,11 +79,11 @@ class SklearnClassifierSearchSpace:
         if scaler_standard:
             scalers += [StandardScaler()]
 
-        assert encoder_one_hot or encoder_ordineral
+        assert encoder_one_hot or encoder_ordinal
         encoders = []
         if encoder_one_hot:
             encoders += [OneHotEncoder()]
-        if encoder_ordineral:
+        if encoder_ordinal:
             encoders += [hlp.sklearn_pipeline.CustomOrdinalEncoder()]
 
         return {
@@ -91,11 +95,11 @@ class SklearnClassifierSearchSpace:
     @staticmethod
     def _search_space_logistic(solver='lbfgs',
                                max_iter=1000,
-                               C=(1e-6, 1e+2),
-                               C_prior='log-uniform',
-                               imputer_strategies=['mean', 'median', 'most_frequent'],
+                               C=(1e-6, 1e+2),  # noqa
+                               C_prior='log-uniform',  # noqa
+                               imputer_strategies=['mean', 'median', 'most_frequent'],  # noqa
                                random_state=None):
-        from skopt.space import Real, Categorical, Integer
+        from skopt.space import Real, Categorical
 
         model = LogisticRegression(
             solver=solver,
@@ -108,7 +112,7 @@ class SklearnClassifierSearchSpace:
             'model__C': Real(C[0], C[1], prior=C_prior),
         }
         # these steps correspond to the pipeline built in `build_classifier_search_pipeline()`
-        logistic_search_space.update(self._build_transformer_search_space(
+        logistic_search_space.update(ClassifierSearchSpace._build_transformer_search_space(
             imputer_strategies=imputer_strategies,
         ))
 
@@ -117,13 +121,20 @@ class SklearnClassifierSearchSpace:
     @staticmethod
     def _search_space_xgboost(eval_metric='logloss',
                               use_label_encoder=False,
-                              max_depth = (3, 10),
-                              n_estimators = (50, 1000),
-                              learning_rate = (0.01, 0.3),
-                              colsample_bytree = (0.01, 1),
-                              subsample = (0.1, 1),
-                              imputer_strategies=None,
+                              max_depth=(3, 10),
+                              n_estimators=(50, 1000),
+                              learning_rate=(0.01, 0.3),
+                              colsample_bytree=(0.01, 1),
+                              subsample=(0.1, 1),
+                              imputer_strategies=['mean', 'median', 'most_frequent'],  # noqa
                               random_state=None):
+        """
+        #`XGBoostError: XGBoost Library (libxgboost.dylib) could not be loaded on Apple Silicon (ARM)`
+        #https://github.com/dmlc/xgboost/issues/6909
+        #```
+        #pip install --upgrade --force-reinstall xgboost --no-binary xgboost -v
+        #```
+        """
         from skopt.space import Real, Categorical, Integer
         from xgboost import XGBClassifier
 
@@ -139,7 +150,9 @@ class SklearnClassifierSearchSpace:
         # colsample_bytree: 0.5–1
         # subsample: 0.6–1
         # Then, you can focus on optimizing max_depth and n_estimators.
-        #You can then play along with the learning_rate, and increase it to speed up the model without decreasing the performances. If it becomes faster without losing in performances, you can increase the number of estimators to try to increase the performances.
+        # You can then play along with the learning_rate, and increase it to speed up the model without
+        # decreasing the performances. If it becomes faster without losing in performances, you can increase
+        # the number of estimators to try to increase the performances.
         xgb_search_space = {
             'model': Categorical([model]),
             'model__max_depth': Integer(max_depth[0], max_depth[1]),
@@ -149,7 +162,7 @@ class SklearnClassifierSearchSpace:
             'model__subsample': Real(subsample[0], subsample[1]),
         }
         # these steps correspond to the pipeline built in `build_classifier_search_pipeline()`
-        xgb_search_space.update(self._build_transformer_search_space(
+        xgb_search_space.update(ClassifierSearchSpace._build_transformer_search_space(
             imputer_strategies=imputer_strategies,
             scaler_min_max=False,
             scaler_standard=False,
@@ -161,9 +174,9 @@ class SklearnClassifierSearchSpace:
         search_spaces = []
         for model_enum, num_iterations in zip(self._models, self._iterations):
             if model_enum == ClassifierSearchSpaceModels.LogisticRegression:
-                space = SklearnClassifierSearchSpace._search_space_logistic()
+                space = ClassifierSearchSpace._search_space_logistic()
             elif model_enum == ClassifierSearchSpaceModels.XGBoost:
-                space = SklearnClassifierSearchSpace._search_space_xgboost()
+                space = ClassifierSearchSpace._search_space_xgboost()
                 
             else:
                 assert False
@@ -173,33 +186,9 @@ class SklearnClassifierSearchSpace:
         return search_spaces
     
     def param_name_mappings(self):
-        pass
+        temp = self._models
 
-
-    def search_space_param_name_mapping_classifier_pipeline():
-        return {
-            'prep__non_numeric__encoder__transformer': 'encoder',
-            'prep__numeric__imputer__transformer': 'imputer',
-            'prep__numeric__scaler__transformer': 'scaler'
-        }
-
-    def search_space_param_name_mapping_classifier_logistic():
-        return {
-            'model__C': 'C',
-        }
-
-    def search_space_param_name_mapping_classifier_xgboost():
-        return {
-            'model__max_depth': 'max_depth',
-            'model__n_estimators': 'n_estimators',
-            'model__learning_rate': 'learning_rate',
-            'model__colsample_bytree': 'colsample_bytree',
-            'model__subsample': 'subsample',
-            'model': 'model',
-        }
-
-
-
+        return temp
 
     def __str__(self):
         return 'asdf'
