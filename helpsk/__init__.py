584ba316f63e3fe6bd9bb847e666b60b7086ac9e
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-02-19 13:50:08 -0800

    refactor

--- helpsk/__init__.py
+++ helpsk/__init__.py
@@ -13,6 +13,7 @@ import helpsk.pandas_style
 import helpsk.plot
 import helpsk.sklearn_eval
 import helpsk.sklearn_search_base
+import helpsk.sklearn_search_bayesian
 import helpsk.sklearn_search_bayesian_base
 import helpsk.sklearn_search_bayesian_classification
 import helpsk.sklearn_pipeline
